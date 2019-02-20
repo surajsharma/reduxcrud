37c06cc9f52298773e0a8e7628694cf29c41fd2e
Suraj Sharma <www.surajsharma@gmail.com>
2019-02-20 12:24:22 +0530

    Updates

---
+++ precache-manifest.ce1fd02233928391b2f13905641300d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f19307203d1d4f35a0e0",
+    "url": "/reduxcrud/static/css/main.27cc140b.chunk.css"
+  },
+  {
+    "revision": "f19307203d1d4f35a0e0",
+    "url": "/reduxcrud/static/js/main.f1930720.chunk.js"
+  },
+  {
+    "revision": "00740c74f37d84b9a97a",
+    "url": "/reduxcrud/static/js/runtime~main.00740c74.js"
+  },
+  {
+    "revision": "7151523fd8baea9a65c9",
+    "url": "/reduxcrud/static/js/2.7151523f.chunk.js"
+  },
+  {
+    "revision": "363a7075efb4ed79097ae03bce7b120c",
+    "url": "/reduxcrud/index.html"
+  }
+];
